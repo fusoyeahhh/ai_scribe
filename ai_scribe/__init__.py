df24628e0ebaaae79c4b05161c8dfad7160b719e
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-09-02 18:42:39 -0700

    ai_scribe: remove unused (and conflicting) import

--- ai_scribe/__init__.py
+++ ai_scribe/__init__.py
@@ -1,6 +1,5 @@
 from . import command_graph
 from . import scripting
-from .extract import *
 from .pack import _validate, randomize_scripts
 
 from .themes import AREA_SETS, STATUS_THEMES, ELEM_THEMES, BOSSES
