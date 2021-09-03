4becd1fce7d2e5cca7d9a4ab3cc39050361cb6eb
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-09-02 19:51:48 -0700

    ai_scribe: remove unused imports

--- ai_scribe/__init__.py
+++ ai_scribe/__init__.py
@@ -1,9 +1,3 @@
-from . import command_graph
-from . import scripting
-from .pack import _validate, randomize_scripts
-
-from .themes import AREA_SETS, STATUS_THEMES, ELEM_THEMES, BOSSES
-
 # These enemies do not have names in the ROM
 _NAME_ALIASES = {
     "": "Terra In Flashback",
