058fc79f52f9ba95913442adc2865044813130d7
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-09-05 12:30:42 -0700

    extract.py: enhance script extraction
      - check for non-vanilla script pointers
      - partition and handle them separately
        - use heuristic to determine their extent and content
      - do not alias names if they've been changed

--- ai_scribe/extract.py
+++ ai_scribe/extract.py
@@ -8,11 +8,16 @@ from . import _NAME_ALIASES
 from . import scripting
 from .scripting import translate, _CHARS
 
+# Allow no more than this number of 0xFF bytes in a potential script
+_MAX_FF_TOLERANCE = 16
+
 def get_subgraph(g, nodes=None):
     import functools
     nodes = nodes or functools.reduce(list.__add__, [list(g[cmd]) for cmd in SYNTAX])
     return g.subgraph(nodes)
 
+def non_vanilla_ptrs(script_ptrs):
+    return [ptr for ptr in script_ptrs if ptr not in range(0xF8700, 0xFC050)]
 
 def detect_bc(script_ptrs):
     """
@@ -20,21 +25,7 @@ def detect_bc(script_ptrs):
     established in vanilla, we detect this to determine whether BC
     has already touched the scripts or not
     """
-    oor_ptr = [ptr for ptr in script_ptrs
-                           if ptr not in range(0xF8900, 0xFC24F)]
-    return len(oor_ptr) == 0
-
-def extract_scripts_bc():
-    # FIXME: assumes bc libraries are imported
-    try:
-        from monsterrandomizer import get_monsters
-    except ImportError as e:
-        print(e)
-        exit("BC libraries are required to access scripts in a pre-randomized ROM.")
-    return {ent.name: ent.aiscript for ent in get_monsters()}
-    # NOTE: for future reference
-    # m.set_relative_ai(pointer)
-    # m.aiscript = aiscript
+    return len(non_vanilla_ptrs(script_ptrs)) > 0
 
 _FIX_MAG_ROADER = True
 def _check_and_fix_script_exceptions(name, script):
@@ -42,10 +33,8 @@ def _check_and_fix_script_exceptions(name, script):
         script._bytes += b'\xFF'
         log.info("Mag Roader4 is known to have a script bug: it has no ending byte. Fix requested and applied")
         scripting.Script.validate(script._bytes)
-    elif name == "13":
-        pass
     else:
-        raise ValueError("Got invalid script [{name}] with no known fix.")
+        raise ValueError(f"Got invalid script [{name}] with no known fix.")
 
     return script
 
@@ -54,6 +43,18 @@ def extract_scripts(romfile, script_ptrs, names, unused_bytes=7):
 
     scripts = dict(zip(script_ptrs, names))
     scripts = dict(sorted(scripts.items(), key=lambda t: t[0]))
+    _ptrs = scripts.copy()
+
+    non_std_ptrs = non_vanilla_ptrs(script_ptrs)
+    non_std_ptrs = {ptr: name for ptr, name in scripts.items()
+                                            if ptr in non_std_ptrs}
+
+    # We don't know if these bytes are used or not
+    if len(non_std_ptrs) > 0:
+        unused_bytes = 0
+        scripts = {ptr: name for ptr, name in scripts.items()
+                                           if ptr not in non_std_ptrs}
+
     # clip unused bytes at the end of the block
     script_ptrs = [*scripts] + [0xFC050 - unused_bytes]
 
@@ -73,14 +74,68 @@ def extract_scripts(romfile, script_ptrs, names, unused_bytes=7):
         assert s._bytes == scripts[name]
         scripts[name] = s
 
+    # Handle scripts in nonstandard locations
+    for ptr, name in non_std_ptrs.items():
+        log.info(f"Non standard pointer location {hex(ptr)} -> {name}")
+        # heuristic: count number of end blocks
+        # This will be confused by the appearance of "Nothing" in skill selection lists
+        # So, we try to acquire blocks until it "makes sense"...
+        eptr, _script = ptr, b''
+        while _script.count(b'\xFF') <= _MAX_FF_TOLERANCE:
+            eptr = romfile.index(b'\xFF', eptr) + 1
+            _script = romfile[ptr:eptr]
+
+            # Try to validate
+            try:
+                scripting.Script.validate(_script)
+            except Exception as e:
+                log.debug(e)
+                continue
+
+            # If validation succeeds, and we have two valid blocks
+            # we have a valid script
+            script = scripting.Script(_script, name)
+            try:
+                # FIXME: can't check for nff >= 2, condition can be triggered
+                # by incomplete scripts
+                if script.translate().count("END BLOCK") == 2:
+                    break
+            except Exception as e:
+                log.debug(e)
+                continue
+
+        # This is only executed if we hit the maximum tolerance,
+        # which is a failure condition
+        else:
+            log.error(f"Script for {name} cannot be properly parsed; "
+                      "it is likely that this is an invalid script. "
+                      "If possible, script follows:")
+            log.error(script.translate(allow_partial=True, memblk=True))
+            exit()
+
+        if eptr - ptr >= 1000:
+            log.warning(f"Script for {name} is very long ({eptr - ptr} bytes); "
+                        "it is likely that this is an invalid script.")
+
+        scripts[name] = script
+
     # Handle scripts with known bugs / odd features
     for name, script in invalid.items():
-        invalid[name] = _check_and_fix_script_exceptions(name, script)
+        try:
+            invalid[name] = _check_and_fix_script_exceptions(name, script)
+        except Exception as e:
+            # Vanilla and no recourse, so we abort
+            if len(non_std_ptrs) == 0:
+                raise e
+            log.warning(f"Script for [{name}] seems to be invalid, but with no specified fix. "
+                        "Because BC has already modified the scripts, we assume this is non-fatal "
+                        "and will take no further action. This may cause future problems.")
 
     return scripts
 
 def extract(romfile=None, return_names=False):
     romfile = "Final Fantasy III (U) (V1.0) [!].smc"
+    #romfile = "base_roms/Final_Fantasy_3_Textless.1620609722.smc"
 
     with open(romfile, "rb") as fin:
         romfile = fin.read()
@@ -105,17 +160,19 @@ def extract(romfile=None, return_names=False):
             i += 1
             _name = name + str(i)
 
-    # Alias blank names to something more useful
-    names = [_NAME_ALIASES.get(n, n) for n in names]
-
-    names = dict(zip(names, script_ptrs))
     # Detect if BC has changed the scripts or their structure in some way
     is_bc = detect_bc(script_ptrs)
     log.info(f"ROM type: {'bc' if is_bc else 'vanilla'}")
-    if is_bc:
-        scripts = extract_scripts_bc()
-    else:
-        scripts = extract_scripts(romfile, script_ptrs, names)
+
+    # Alias blank names to something more useful
+    # FIXME: can't alias names if they've been changed
+    # Moreover, since the renaming is order dependent ('kefka2' -> ...)
+    # we can't even alias bosses consistently
+    if not is_bc:
+        names = [_NAME_ALIASES.get(n, n) for n in names]
+
+    names = dict(zip(names, script_ptrs))
+    scripts = extract_scripts(romfile, script_ptrs, names)
 
     with open("script_dump.txt", "w") as fout:
         for i, (name, script) in enumerate(scripts.items()):
@@ -124,6 +181,18 @@ def extract(romfile=None, return_names=False):
     if return_names:
         return scripts, names
     return scripts
+
+# Unused, could be in the future if fully integrated with BC
+def extract_scripts_bc():
+    try:
+        from monsterrandomizer import get_monsters
+    except ImportError as e:
+        print(e)
+        exit("BC libraries are required to access scripts in a pre-randomized ROM.")
+    return {ent.name: ent.aiscript for ent in get_monsters()}
+    # NOTE: for future reference
+    # m.set_relative_ai(pointer)
+    # m.aiscript = aiscript
 
 if __name__ == "__main__":
     extract()
