264dd3ca5e39834ccff7ff85ccde9b7233070379
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-10-10 13:51:13 -0700

    ai_scribe: remove premature debugging exit

--- ai_scribe/__main__.py
+++ ai_scribe/__main__.py
@@ -415,4 +415,3 @@ if __name__ == "__main__":
                     log.warning(f"{hex(scr.ptr)} <-> {hex(export[n].ptr)}")
                     log.warning(tableau_scripts(scr.translate(), export[n].translate()))
                 assert scr._bytes == export[n]._bytes, n
-        exit()
