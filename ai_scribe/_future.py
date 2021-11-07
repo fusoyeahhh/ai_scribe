ec6c0f8d15d169af13a96bfe582da0d8ebebe310
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-11-07 15:20:43 -0800

    _future.py: ideas in development

--- ai_scribe/_future.py
+++ ai_scribe/_future.py
@@ -55,3 +55,97 @@ def generate_status_theme(skills, _status):
                 continue
             if random.randint(0, p2) < p1:
                 g.add_edge(i, i2)
+
+def to_script_nodes(script):
+    from .scripting import _CMD_REF,
+    from .syntax import SYNTAX
+    script = [*script].copy()
+
+    cmd_nodes = []
+    while len(script) > 0:
+        v = script.pop(0)
+
+        cmd_node = _CMD_REF["_"]() if v not in SYNTAX else _CMD_REF[v]()
+        script = cmd_node.consume_args(script)
+        cmd_nodes.append(cmd_node)
+
+    return cmd_nodes
+
+try:
+    """
+    from colorama import init, Fore, Back, Style
+    COLORIZE = {
+        0xF0: (Fore.BLUE, Style.RESET_ALL),
+        0xFC: (Style.BRIGHT + Fore.GREEN, Style.RESET_ALL),
+        0xFD: (Style.DIM, Style.RESET_ALL),
+        0xFE: (Fore.LIGHTGREEN, Style.RESET_ALL),
+        0xFF: (Back.WHITE, + Fore.BLACK, Style.RESET_ALL)
+    }
+    """
+except ImportError:
+    #log.warning("Colorized output requires 'colorama' to be installed. Proceeding without.")
+    COLORIZE = {}
+    def translate(script, memblk=False, allow_partial=False, colorize=False):
+        pass
+
+
+class Cmd:
+    """
+    Abstraction of a script command and its arguments. Used as nodes in more complicated scripting constructions.
+    """
+
+    def translate_args(self, *args):
+        for a, t in zip(args, self._argument_types):
+            try:
+                descr = t[a] if isinstance(t, (dict, list)) else t(a)
+            except TypeError:
+                pass
+            except KeyError:
+                # There's at least one missing targeting byte description
+                pass
+        return descr
+
+    def consume_args(self, scr_bytes):
+        """
+        Consume the requisite number of bytes from the script to determine the arguments to the command byte.
+        """
+        return scr_bytes[:self.nargs]
+
+
+class ScriptFragment:
+    """
+    Compositions of script fragments.
+    """
+    _variables = {}
+
+    def __init__(self, byteval, descr):
+        self.byteval = byteval
+        self.descr = descr
+
+    @classmethod
+    def register_variable(cls, var):
+        if var in ScriptFragment._variables:
+            raise ValueError(f"{var} is already registered.")
+        ScriptFragment._variables[var] = None
+
+    @classmethod
+    def acquire_new_variable(cls):
+        # FIXME: how many variable registers do we have?
+        for i in range(255):
+            if i not in ScriptFragment._variables:
+                ScriptFragment.register_variable(i)
+                return i
+
+        raise ValueError("No variable registers available.")
+
+    def expand(self):
+        pass
+
+    def to_script(self):
+        pass
+
+class OneTimeTrigger(ScriptFragment):
+    def __init__(self):
+        fire_once = [*b'\xfc\x15\x03\x00\xf9\x00\x03\x00']
+        script = [CmdPred(), VarManip()]
+        super()
