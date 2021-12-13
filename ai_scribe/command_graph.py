4cf6850b1fc7a62832af9453a3d546e93f7b4730
fusoyeahhh <73078635+fusoyeahhh@users.noreply.github.com>
2021-12-12 20:38:10 -0800

    command_graph.py: rewrite script generator engine
    Mainly done with applying rules in mind. We'd have to rejection sample entire scripts under the old framework in CommandGraph otherwise.

--- ai_scribe/command_graph.py
+++ ai_scribe/command_graph.py
@@ -1,4 +1,5 @@
 import random
+from collections import defaultdict
 
 import networkx
 import numpy
@@ -509,13 +510,34 @@ class RestrictedCommandGraph(CommandGraph):
             [((None, None), (0xF6, {...}))],
     }
 
+    class Rule:
+        def __init__(self, seq1, seq2):
+            self._seq1, self._seq1 = seq1, seq2
+
+        def _maybe_expand(self, c1, c2):
+            return c1, c2
+
+        def check_rule(self, cmd1=None, cmd2=None, arg1=None, arg2=None):
+            # an existing link disallows that connection
+            # FIXME: could be slow
+            for ((c1, a1), (c2, a2)) in self._maybe_expand(cmd1, cmd2):
+                if (c1 == cmd1 or c1) and (c2 == cmd2 or c2) and \
+                        (arg1 in a1 or {arg1}) and (arg2 in a2 or {arg2}):
+                    break
+            else:
+                return False
+            return True
+
+        def __getitem__(self, ls1, ls2):
+            return self.check_rule(ls1[0], ls2[0], ls1[1], ls2[1])
+
     @classmethod
-    def get_rule_set(cls, *args):
-        rule_map = cls()
+    def get_rule_set(cls, *args, **kwargs):
+        rule_map = kwargs.get("graph", cls())
         args = set(args)
 
-        for c1, c2 in [v for k, v in cls.RULESETS.items() if k in args]:
-            rule_map.rules.add_edge(c1, c2)
+        rule_map.rules = [v for k, v in cls.RULESETS.items()
+                                if k in args]
 
         return rule_map
 
@@ -524,26 +546,177 @@ class RestrictedCommandGraph(CommandGraph):
         if g is not None:
             self.cmd_graph = g.cmd_graph
             self.cmd_arg_graphs = g.cmd_arg_graphs
-        self.rules = networkx.DiGraph()
+        self.rules = []
 
-    def check_rule(self, cmd1, cmd2, arg1=None, arg2=None):
-        # an existing link disallows that connection
-        # FIXME: could be slow
-        for ((c1, a1), (c2, a2)) in self._maybe_expand(cmd1, cmd2):
-            if (c1 == cmd1 or c1) and (c2 == cmd2 or c2) and \
-               (arg1 in a1 or {arg1}) and (arg2 in a2 or {arg2}):
+    def check_rule(self, cmd1=None, cmd2=None, arg1=None, arg2=None):
+        for rule in self.rules:
+            if rule[cmd1, cmd2, arg1, arg2]:
                 break
         else:
             return False
         return True
 
-    """
-    # FIXME: need to separate into generator and driver method
-    def generate_from_graph(self, start_cmd="^",
-                            main_block_len=None, main_block_avg=2, allow_empty_main_blocks=False,
-                            disallow_commands=set(), weighted=True, naborts=20, strict=True):
-        pass
-    """
+    def generate_script_token(self, history, **kwargs):
+        # Possibility #1, need to change -1 to be last command
+        kwargs["main_block_len"] = 1
+        # TODO: actually check the result
+        return self.generate_from_graph(start_cmd=history[-1], **kwargs)
+
+    # rewrite of generate_from_graph
+    def _generate_from_graph(self, start_cmd="^",
+                             main_block_len=None, main_block_avg=2, allow_empty_main_blocks=False,
+                             cntr_block_len=None, cntr_block_avg=0, allow_empty_cntr_blocks=True,
+                             disallow_commands=set(), weighted=True, naborts=20, strict=True):
+
+        main_block_avg = None if main_block_avg is None else numpy.random.poisson(main_block_avg)
+        main_block_len = main_block_len or main_block_avg
+        if not allow_empty_main_blocks and main_block_len == 0:
+            main_block_len = 1
+
+        cntr_block_avg = None if cntr_block_avg is None else numpy.random.poisson(cntr_block_avg)
+        cntr_block_len = cntr_block_len or cntr_block_avg
+        if not allow_empty_cntr_blocks and cntr_block_len == 0:
+            cntr_block_len = 1
+
+        # Make a copy, because we can modify the graph in flight
+        g = self.cmd_graph.copy()
+
+        # TODO: replace these with rule sets
+        # Handle disallowed commands
+        # Basically, we're 'contracting' the graph by allowing the jump to 'skip'
+        # over the disallowed command, while preserving the ability to jump to
+        # outgoing connections of the disallowed node itself
+        for gptr in set(g.nodes) - disallow_commands:
+            for cmd in set(g[gptr]) & disallow_commands:
+                # The contraction process, by default, changes edges between u and v into self loops
+                # on the linked node. While it's not fatal to have this behavior, to keep things
+                # a bit cleaner, we'll ensure the link to the start char is broken before the contraction
+                # so that no self-loops on the start cmd happen
+                if start_cmd in g[cmd]:
+                    g.remove_edge(cmd, start_cmd)
+                if cmd in g[start_cmd]:
+                    g.remove_edge(start_cmd, cmd)
+                # This contracts for only this node, and it's a permanent change for this run through
+                #print(self.cmd_graph.edges)
+                g = networkx.algorithms.minors.contracted_nodes(g, gptr, cmd)
+                #print(gptr, cmd, g.edges)
+
+        context = {
+            "phase": "main",
+            "weighted": weighted,
+            "vars_in_use": set(),
+            "nfc": 0,
+            "ncmd": 0
+        }
+
+        aborts = defaultdict(lambda: 0)
+        while naborts >= 0:
+            script, gptr = [], start_cmd
+            # Allowed number of rule checks per attempt
+            context["rule_checks"] = 10
+            try:
+                scr_len = main_block_len + cntr_block_len
+                while scr_len >= 0:
+                    last = gptr
+                    gptr, args = self._generate_script_token(g, gptr, script_context="main", **context)
+
+                    # Need we arguments to command?
+                    nargs = SYNTAX[gptr][0]
+                    args = []
+                    if gptr in self.cmd_arg_graphs and (nargs or 0) > 0:
+                        # append arguments
+                        args = expand(self.cmd_arg_graphs[gptr], gptr, nargs)
+
+                    # Do rule checking or abort if we're encountering too many
+                    if context["rule_checks"] <= 0:
+                        raise KeyError(f"Rule application failed too many times.")
+                    elif self.check_rule(last, gptr):
+                        # TODO: Get rule broken
+                        #aborts[f"rule/{rule}"] += 1
+                        # FIXME: we may hit this very quickly if the rejection sampling space is large
+                        gptr = last
+                        context["rule_checks"] -= 1
+                        continue
+
+                    script.extend([gptr] + args)
+                    scr_len -= 1
+
+                if gptr == 0xFF:
+                    context["phase"] = "counter"
+
+            except KeyError as e:
+                # Bad command graph links
+                # Too many rule applications
+                # FIXME: should this be fatal?
+                script, gptr = [], start_cmd
+                naborts -= 1
+                aborts[f"command_graph/{e.message}"] += 1
+            except ValueError as e:
+                script, gptr = [], start_cmd
+                naborts -= 1
+                aborts[f"general/{e.message}"] += 1
+            except:
+                # FIXME: This should probably be fatal
+                naborts -= 1
+                # We reset as a backup
+                # gptr = start_cmd
+            else:
+                # We're done, add script terminator
+                script += [0xFF]
+
+        if not naborts >= 0:
+            exit("Failed to generate script within the prescribe number of attempts. "
+                 "Exiting to avoid potential infinite loops")
+        assert naborts >= 0, aborts
+
+        return script
+
+    def _generate_script_token(self, g, gptr="^", script_context="main", context={}):
+        # Things needed for context
+        assert script_context in {"main", "counter"}
+        weighted = context.get("weighted", True)
+
+        if gptr not in g or len(g[gptr]) == 0:
+            raise KeyError(f"Current command pointer ({hex(gptr)} / {SYNTAX[gptr]}) "
+                           "has no outgoing links. " + str(g[gptr]))
+
+        # If "weighted" is turned on, then we use the appropriately normalized connection weights
+        # to assign selection probabilities to each potential next step
+        if weighted:
+            gptr = {c: w.get("weight", 1) for c, w in gptr.items()}
+            weights = sum(gptr.values())
+            weights = [w / weights for w in gptr.values()]
+        else:
+            weights = [1 / len(gptr)] * len(gptr)
+        try:
+            gptr = numpy.random.choice(list(gptr), p=weights)
+        except:
+            raise KeyError("gptr has no valid choices."
+                          f"\ngptr / weights: {gptr} {weights}"
+                          f"\ncommand graph ({len(g)} elements): {str(g.edges)}")
+
+        # catch '_'
+        try:
+            gptr = int(gptr)
+        except ValueError:
+            pass
+
+        # Track number of useable commands
+        context["ncmd"] += 1 if gptr in {0xF0, 0xF4, 0xF6, "_"} else 0
+        # Track vars in use
+        # TODO: move to validation
+        if gptr in {0xF8, 0xF9}:
+            context["vars_in_use"].add(args[-2])
+        if gptr in {0xFC} and args[-3] in {0xC, 0xD, 0x14, 0x15}:
+            var = args[-2]
+            # FIXME: rearrange to not do this arbitrarily
+            # Have FC pointers?
+            if len(context["vars_in_use"]) == 0:
+                context["vars_in_use"].add(0x0)
+            if var not in context["vars_in_use"]:
+                var = numpy.random.choice(list(context["vars_in_use"]))
+
+        return gptr, args
 
     def regulate_difficulty(self, diff_thresh=1, diff_val=1):
         from . import themes
